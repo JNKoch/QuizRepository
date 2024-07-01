fcf046feb2d1cb4aa0dd9bf77a0460466034e758
jnk <Janniklaskoch04@gmail.com>
2024-07-01 09:53:21 +0200

    The First Commit

--- Quiz_Covis/src/DasQuiz3/Main.java
+++ Quiz_Covis/src/DasQuiz3/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.start();
-
+        System.out.println("The First Commit");
     }
 }
